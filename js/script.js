f523dbf4dc4147dfe60e754744d11c49f963c69e
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2022-09-30 15:49:58 +0930

    backup to rollback

--- js/script.js
+++ js/script.js
@@ -11,55 +11,94 @@ function startQuiz(event) {
 
 function handleButtonClick() {
   //console.log(handleButtonClick.name);
-  
+
   var displayAnswer = document.getElementById("answerArea");
   displayAnswer.textContent = "Sorry: Wrong answer";
   //displayAnswer.className = "answerPanel";
   displayAnswer.append(displayAnswer);
 }
+//answer 0 as an element
+const questions = [
+  {
+    question: "Q1. Inside which HTML element do you write JavaScript?",
+    correctAnswer: 2,
+    options: [
+      "1. <scripting> </scripting>",
+      "2. <javascripting> </javascripting>",
+      "3. <script> </script>",
+      "4. <js> </js>",
+    ],
+  },
+  {
+    question: "Q2. How do you create a function in Javascript?",
+    correctAnswer: 2,
+    options: [
+      "1. function = myFunction()",
+      "2.function myFunction()",
+      "3. function: myFunction()",
+      "4. myFunction()",
+    ],
+  },
+];
 
+function displayQuestion() {
 
-function displayQuestions() {
+  
   //define questions array
-  let question1 = [
-    { Q: "Q1. Inside which HTML element do you write JavaScript?"},
-      { A1: "1. <scripting> </scripting>"},
-      { A1: "2. <javascripting> </javascripting>"},
-      { A1: "3. <script> </script>", correct: true },
-      { A1: "4. <js> </js>"}];
-  let question2 = [
-      { Q: "Q2. How do you create a function in Javascript?"},
-      { A1: "1. function = myFunction()"},
-      { A1: "2. function myFunction()"},
-      { A1: "3. function: myFunction()",correct:false},
-      { A1: "4. myFunction()"}];
+  // let question1 = [
+  //   { Q: "Q1. Inside which HTML element do you write JavaScript?" },
+  //   { A1: "1. <scripting> </scripting>" },
+  //   { A1: "2. <javascripting> </javascripting>" },
+  //   { A1: "3. <script> </script>", correct: true },
+  //   { A1: "4. <js> </js>" },
+  // ];
+  // let question2 = [
+  //   { Q: "Q2. How do you create a function in Javascript?" },
+  //   { A1: "1. function = myFunction()" },
+  //   { A1: "2. function myFunction()" },
+  //   { A1: "3. function: myFunction()", correct: true },
+  //   { A1: "4. myFunction()" },
+  // ];
 
-  var questionElement = document.getElementById("questionDisplayed");
-  var answerElementOL = document.getElementById("answerDisplayed");
-  //var answerElementLI = document.createElement("li");
-    questionElement.textContent = question1[0].Q;
-    generatedQuestions.append(questionElement);
+  // var questionElement = document.getElementById("questionDisplayed");
+  // var answerElementOL = document.getElementById("answerDisplayed");
+  // //var answerElementLI = document.createElement("li");
+  // questionElement.textContent = question1[0].Q;
+  // generatedQuestions.append(questionElement);
 
-    for (i = 1; i < question1.length; i++) {
-      console.log ("Element " + i + " of array: "+ question1[i].A1);
-      var answerElementLI = document.createElement("li");
-      var answerX= document.getElementById("dynamicQuestions");
-      answerElementLI.textContent = question1[i].A1;//display the value only
-      answerElementLI.setAttribute = ("id",i);//not being assigned
-      answerElementLI.className = "button";
-      
-      answerX.appendChild(answerElementLI);
-      //check for key clicks
-      answerElementLI.addEventListener('click', (e)=>{
-        console.log ("keypressed: "+ e.target.id);
+  // for (i = 1; i < question1.length; i++) {
+  //   //console.log("Element " + i + " of array: " + question1[i].A1);
+  //   var answerElementLI = document.createElement("li");
+  //   var answerX = document.getElementById("answerDisplayed");
+  //   answerElementLI.textContent = question1[i].A1; //display the value only
+  //   answerElementLI.setAttribute("id", i); //assign an id to the <LI> element,
+  //   answerElementLI.className = "button";
 
-      });
-    }
-  }
+  //   answerX.appendChild(answerElementLI);
+  //   //check for key clicks and which element is clicked
+  //   answerElementLI.addEventListener("click", (e) => {
+  //console.log("keypressed: " + e.target.id); //display the key pressed
+  //check what is pressed
+  //if the key pressed == True flag then
+  //display ANSWER CORRECT
+  //SCORE++
+  //Else display ANSWER INCORRECT
+  //display ANSWER WRONG
+  //TIMELEFT--
+  //display next question
+
+  //suggestion from Matt W:
+  //--------------------------
+  // loop over questions object
+  // for each question, set the question title
+  // for each option in this question, set the button text etc
+  // add an event listener that looks at question.correctAnswer value and compares it to the index of the one that was one clicked to determine if it was correct.
   
-   //index = index + 1;
- 
-  
+  //});
+}
+//}
+
+//index = index + 1;
 
 /*        function displayQuestion01() {
           //Display Question 01
